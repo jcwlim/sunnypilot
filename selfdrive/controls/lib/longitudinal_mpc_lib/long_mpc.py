6056f3be64eff74fc9ed98628c6e062c8a8ee2e7
jcwlim <jforum@me.com>
2024-08-28 07:34:33 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -98,7 +98,7 @@ def get_dynamic_personality(v_ego, personality=custom.LongitudinalPersonalitySP.
     x_vel =  [0,    27.69, 27.7]
     y_dist = [1.45, 1.45,  1.40]
   elif personality==custom.LongitudinalPersonalitySP.aggressive:
-    x_vel =  [0,  1, 5, 10, 20.0, 20.01, 27.69, 27.7]
+    x_vel =  [0,  1, 5, 10, 13, 20.0, 20.01, 27.69, 27.7]
     y_dist = [-1.07, -1.0, -0.5 , 0.5, 0.8, 1.07, 1.12,  1.12,  1.20]
   else:
     raise NotImplementedError("Dynamic personality not supported")
