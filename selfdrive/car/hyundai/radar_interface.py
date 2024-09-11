cb958059799f5d81e1d23fffcc47bb97412fa491
jcwlim <jforum@me.com>
2024-09-12 07:10:32 +0800

    Update radar_interface.py

--- selfdrive/car/hyundai/radar_interface.py
+++ selfdrive/car/hyundai/radar_interface.py
@@ -48,7 +48,7 @@ class RadarInterface(RadarInterfaceBase):
     self.previous = 0
     self.counter = 0
     self.dRelFilter = StreamingMovingAverage(2)
-    self.vRelFilter = StreamingMovingAverage(13)
+    self.vRelFilter = StreamingMovingAverage(10)
 
     self.radar_off_can = CP.radarUnavailable
     self.rcp = get_radar_can_parser(CP)
@@ -118,7 +118,7 @@ class RadarInterface(RadarInterfaceBase):
 
       dRel = msg['ACC_ObjDist']
       vRel = msg['ACC_ObjRelSpd']
-      valid = msg['ACC_ObjStatus'] and 18 < dRel < 150
+      valid = msg['ACC_ObjStatus'] and dRel < 150 #18 < dRel < 150
       # self.counter += 1
       # if self.counter % 5 == 0:
       #   valid = True
