e8e8e1e74874f339951c874b5e495223ce3f9447
jcwlim <jforum@me.com>
2024-09-12 08:22:40 +0800

    Update radar_interface.py

--- selfdrive/car/hyundai/radar_interface.py
+++ selfdrive/car/hyundai/radar_interface.py
@@ -47,7 +47,7 @@ class RadarInterface(RadarInterfaceBase):
     self.track_id = 0
     self.previous = 0
     self.counter = 0
-    self.dRelFilter = StreamingMovingAverage(5)
+    self.dRelFilter = StreamingMovingAverage(2)
     self.vRelFilter = StreamingMovingAverage(10)
 
     self.radar_off_can = CP.radarUnavailable
