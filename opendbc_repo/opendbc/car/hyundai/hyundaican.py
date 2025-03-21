de6b1e756e3c248644ff49317b2c5c7b0795779e
jasonlim <jforum@me.com>
2025-03-21 15:12:36 +0800

    Adjust minor jerk

--- opendbc_repo/opendbc/car/hyundai/hyundaican.py
+++ opendbc_repo/opendbc/car/hyundai/hyundaican.py
@@ -173,8 +173,8 @@ def create_acc_commands(packer, enabled, accel, upper_jerk, idx, hud_control, se
     return {
       "ComfortBandUpper": 0.0, # stock usually is 0 but sometimes uses higher values
       "ComfortBandLower": 0.0, # stock usually is 0 but sometimes uses higher values
-      "JerkUpperLimit": upper_jerk, # stock usually is 1.0 but sometimes uses higher values
-      "JerkLowerLimit": 5.0, # stock usually is 0.5 but sometimes uses higher values
+      "JerkUpperLimit": 1.0, #upper_jerk, # stock usually is 1.0 but sometimes uses higher values
+      "JerkLowerLimit": 0.5, #5.0, # stock usually is 0.5 but sometimes uses higher values
       "ACCMode": 2 if enabled and long_override else 1 if enabled else 4, # stock will always be 4 instead of 0 after first disengage
       "ObjGap": 2 if hud_control.leadVisible else 0, # 5: >30, m, 4: 25-30 m, 3: 20-25 m, 2: < 20 m, 0: no lead
     }
