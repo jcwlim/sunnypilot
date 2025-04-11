d1017fb0b0efee1b1c84b2cf6d82b46e2b780f9e
jcwlim <jforum@me.com>
2025-04-11 19:44:01 +0800

    Previous was very good but let’s try 1 and 2

--- opendbc_repo/opendbc/sunnypilot/car/hyundai/escc.py
+++ opendbc_repo/opendbc/sunnypilot/car/hyundai/escc.py
@@ -100,11 +100,11 @@ class EsccRadarInterfaceBase:
 
       #valid = msg['ACC_ObjStatus']
       drel = msg['ACC_ObjDist']
-      drel += min(3, 1 + max(0, (drel - 7) / 4))
+      drel += min(2, 1 + max(0, (drel - 7) / 4))
       vrel = msg['ACC_ObjRelSpd']
 
       reset_pts = abs(drel - self.previous) > 3 #or abs(vrel - self.prev_vRel) > 1
-      if drel <= 18 and drel >=4 and vEgo_km <= 40:
+      if drel <= 18 and drel >=6 and vEgo_km <= 40:
         reset_pts = True
 
       valid = False
